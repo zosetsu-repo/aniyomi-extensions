d2c2f4635beb9f4788c9d5ccf34da2baab94fb03
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2025-01-12 12:43:13 +0500

    [ci] build only required modules (#6900)
    * workflow
    
    * configs
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * rewrite python scripts
    
    * small tweak
    
    * Update .github/scripts/modules-to-build.py
    
    Co-authored-by: AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
    
    * set full path of git
    
    * now?
    
    * i hate my life
    
    * and should work now
    
    * And some more changes
    
    * And some more changes 2
    
    * Oops
    
    * support for always building some modules
    
    * string replace fix
    
    * don't run matrix chunk if empty
    
    * bruh moment
    
    * revert
    
    * test pr workflow
    
    - remove extension
    - modify extension
    - modify multisrc
    
    * fix gradlew command
    
    * Revert "test pr workflow"
    
    This reverts commit 8c3aa34555a26f2257d2d791104eb63bbd48856c.
    
    * skip step
    
    * skip step x2
    
    * perhaps
    
    * make if work?
    
    * Reapply "test pr workflow"
    
    This reverts commit 9c7f2a7acd787b18b98ef99da733cc2f8077b2a7.
    
    * make if work!
    
    * Revert "Reapply "test pr workflow""
    
    This reverts commit 43238b0d7bfaa81b8067c7206bb9fb081fa4dbba.
    
    * update job name
    
    * Revert "update job name"
    
    This reverts commit 361b6e7279b901a78fac3eb77e909f480cf54475.
    
    * final changes
    
    * Cleanup some common code
    
    * Cleanup settings.gradle.kts
    
    * Reapply "Reapply "test pr workflow""
    
    This reverts commit 68524b649974e4d05b6a01d0ec297a5f28617071.
    
    * Revert "Reapply "Reapply "test pr workflow"""
    
    This reverts commit bafab8b8405cff9b9142d9f83c4f5adac92da6a0.
    
    * correct delete output name
    
    * pr test: multiple matrices
    
    * Revert "pr test: multiple matrices"
    
    This reverts commit 87fd5fcfd01dc4d83658312293fcc8ac3586086e.
    
    ---------
    
    Co-authored-by: FourTOne5 <107297513+FourTOne5@users.noreply.github.com>
    (cherry picked from commit 14ef26a5a6f417aa6f003a338f1ae79830ec49a2)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,10 @@
-buildscript {
+allprojects {
     repositories {
         mavenCentral()
         google()
-        maven(url = "https://plugins.gradle.org/m2/")
+        maven(url = "https://jitpack.io")
     }
-    dependencies {
-        classpath(libs.gradle.agp)
-        classpath(libs.gradle.kotlin)
-        classpath(libs.gradle.kotlin.serialization)
-        classpath(libs.gradle.kotlinter)
-    }
-}
 
-allprojects {
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
         kotlinOptions {
             jvmTarget = JavaVersion.VERSION_1_8.toString()
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,12 @@
-apply(from = "repositories.gradle.kts")
+/**
+ * Add or remove modules to load as needed for local development here.
+ */
+loadAllIndividualExtensions()
+// loadIndividualExtension("all", "jellyfin")
 
+/**
+ * ===================================== COMMON CONFIGURATION ======================================
+ */
 include(":core")
 
 // Load all modules under /lib
@@ -8,26 +15,9 @@ File(rootDir, "lib").eachDir { include("lib:${it.name}") }
 // Load all modules under /lib-multisrc
 File(rootDir, "lib-multisrc").eachDir { include("lib-multisrc:${it.name}") }
 
-if (System.getenv("CI") != "true") {
-    // Local development (full project build)
-
-    /**
-     * Add or remove modules to load as needed for local development here.
-     */
-    loadAllIndividualExtensions()
-    // loadIndividualExtension("all", "jellyfin")
-} else {
-    // Running in CI (GitHub Actions)
-
-    val chunkSize = System.getenv("CI_CHUNK_SIZE").toInt()
-    val chunk = System.getenv("CI_CHUNK_NUM").toInt()
-
-    // Loads individual extensions
-    File(rootDir, "src").getChunk(chunk, chunkSize)?.forEach {
-        loadIndividualExtension(it.parentFile.name, it.name)
-    }
-}
-
+/**
+ * ======================================== HELPER FUNCTION ========================================
+ */
 fun loadAllIndividualExtensions() {
     File(rootDir, "src").eachDir { dir ->
         dir.eachDir { subdir ->
@@ -37,18 +27,6 @@ fun loadAllIndividualExtensions() {
 }
 fun loadIndividualExtension(lang: String, name: String) {
     include("src:$lang:$name")
-}
-
-fun File.getChunk(chunk: Int, chunkSize: Int): List<File>? {
-    return listFiles()
-        // Lang folder
-        ?.filter { it.isDirectory }
-        // Extension subfolders
-        ?.mapNotNull { dir -> dir.listFiles()?.filter { it.isDirectory } }
-        ?.flatten()
-        ?.sortedBy { it.name }
-        ?.chunked(chunkSize)
-        ?.get(chunk)
 }
 
 fun File.eachDir(block: (File) -> Unit) {
